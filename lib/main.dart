626aac204c33e2f58f910af3d30f0f04f7999c63
AbhishekBansal <bansalabhishek7411@gmail.com>
2023-06-21 11:32:01 +0530

    spalsh

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: RoutesName.profile,
+        initialRoute: RoutesName.splash,
         onGenerateRoute: Routes.generateRoute,
       ),
     );
